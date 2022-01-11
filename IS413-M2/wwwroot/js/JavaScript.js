37efdf4a325e5c5a22e987e4ac6f46492d757136
bryceb105 <90522864+bryceb105@users.noreply.github.com>
2022-01-10 22:32:18 -0700

    Add project files.

---
+++ IS413-M2/wwwroot/js/JavaScript.js
@@ -0,0 +1,36 @@
+﻿var gCalc;
+var gGrade;
+
+$("#btnSend").click(function () {
+    /*calculate grade*/
+    gCalc = ((parseFloat($('#gAssignment').val()) * .55) + (parseFloat($('#gGroup').val()) * .05) + (parseFloat($('#gQuiz').val()) * .1) + (parseFloat($('#gExam').val()) * .2) + (parseFloat($('#gIntex').val()) * .1));
+
+/*    calculate letter grade*/
+    if (gCalc >= 94) {
+        gGrade = "A"
+    } else if (gCalc >= 90) {
+        gGrade = "A-"
+    } else if (gCalc >= 87) {
+        gGrade = "B+"
+    } else if (gCalc >= 84) {
+        gGrade = "B"
+    } else if (gCalc >= 80) {
+        gGrade = "B-"
+    } else if (gCalc >= 77) {
+        gGrade = "C+"
+    } else if (gCalc >= 74) {
+        gGrade = "C"
+    } else if (gCalc >= 70) {
+        gGrade = "C-"
+    } else if (gCalc >= 67) {
+        gGrade = "D+"
+    } else if (gCalc >= 64) {
+        gGrade = "D"
+    } else if (gCalc >= 60) {
+        gGrade = "D-"
+    } else {
+        gGrade = "E"
+    }
+
+    alert("Percentage Grade: " + gCalc.toFixed(2) + "\nLetter Grade: " + gGrade)
+})
